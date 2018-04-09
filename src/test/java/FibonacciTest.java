b0ab4ef10e2c749bf2be3184d72b65da15638200
Mert Serezli <mertserezli@gmail.com>
2018-04-09 14:03:59 +0300

    add assumption test

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,7 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 public class FibonacciTest {
 
@@ -14,5 +17,34 @@ public class FibonacciTest {
     void negativeParameterTest() {
         assertThrows(IllegalArgumentException.class, () -> Fibonacci.fibonacci(-1));
     }
+
+    @Test
+    void assumptionTest() {
+        assumingThat("CI".equals(System.getenv("ENV")),
+            () -> {
+                // perform these assertions only on the CI server
+                assertEquals(2, 2);
+            });
+    }
+
+    @Test
+    void testOnlyOnCiServer() {
+        assumeTrue("CI".equals(System.getenv("ENV")), () -> "Aborting test: not on CI workstation");
+        Assertions.assertEquals(1, Fibonacci.fibonacci(1));
+    }
+
+    @Test
+    void testOnlyOnDeveloperWorkstation() {
+        assumeTrue("DEV".equals(System.getenv("ENV")), () -> "Aborting test: not on developer workstation");
+        Assertions.assertEquals(1, Fibonacci.fibonacci(1));
+    }
+
+    @Test
+    void testInAllEnvironments() {
+        assumingThat("CI".equals(System.getenv("ENV")), () -> assertEquals(1, Fibonacci.fibonacci(1)));
+        // perform these assertions in all environments
+        assertEquals(1, Fibonacci.fibonacci(2));
+    }
+
 
 }
