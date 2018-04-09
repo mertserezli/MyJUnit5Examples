6afa7b9a250a0612b346d5cb13ff097b7a0f0af2
Mert Serezli <mertserezli@gmail.com>
2018-04-09 14:10:10 +0300

    add enabledOn, enabledIf, Tag

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 public class FibonacciTest {
 
+    @Tag("Standard test")
     @Test
     void firstFibonaciNumberTest() {
         assertEquals(1, Fibonacci.fibonacci(1));
     }
 
+    @Tag("Standard test")
     @Test
     void negativeParameterTest() {
         assertThrows(IllegalArgumentException.class, () -> Fibonacci.fibonacci(-1));
@@ -44,6 +53,19 @@ public class FibonacciTest {
         assumingThat("CI".equals(System.getenv("ENV")), () -> assertEquals(1, Fibonacci.fibonacci(1)));
         // perform these assertions in all environments
         assertEquals(1, Fibonacci.fibonacci(2));
+    }
+
+    @EnabledOnOs({ LINUX, MAC })
+    @DisabledOnOs(WINDOWS)
+    @Test
+    void operatingSystemDependentTest(){
+        System.out.println("I only work on linux and mac computers");
+    }
+
+    @Test
+    @EnabledIf("2 * 3 == 6")
+    void willBeExecuted() {
+        System.out.println("I will be executed");
     }
 
 
